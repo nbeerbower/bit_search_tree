4e800a7cb64d1aea4d5a8485234745be578791e5
nbeerbower <nbeerbower@gmail.com>
2017-02-13 00:36:45 -0500

    saving progress of bst implementation

--- bit_search_tree.c
+++ bit_search_tree.c
@@ -7,6 +7,7 @@
 node_t* bst_init() {
 	node_t *root = (node_t *) malloc(sizeof(node_t));
 	root->id = NULL;
+	// TODO: root node meta data
 	root->data = NULL;
 	root->length = 0;
 	root->one = NULL;
@@ -24,23 +25,68 @@ node_t* bst_newNode(void *data, long length) {
 	return node;
 }
 
+void bst_destroyNode(node_t *node, bool recursive) {
+	if (node == NULL) return;
+	
+	free(node->id);
+	node->id = NULL;
+	free(node->data);
+	node->data = NULL;
+	// TODO: finish this
+	
+	if (recursive) {
+		bst_destroyNode(node->zero, true);
+		bst_destroyNode(node->one, true);
+	}
+	
+	//free(node) ??
+}
+
 char* bst_add(node_t* root, node_t *node) {
 	if (root == NULL) {
 		return NULL;
 	}
 	
-	if (root) {
-		//
-	}
+	int level = 0;
+	node_t *cur = root;
+	node_t *next = NULL;
+	byte_t curByte;
+	uint8_t curBit;
+	char *newID = NULL;
+	do {
+		if (level != 0) cur = next;
+		curByte = (byte_t)((byte_t *) node->data)[level/8];
+		curBit = bit_at(curByte, level % 8);
+		node->id = bst_hashID(node->id, curByte, level % 8);
+		next = curBit ? cur->one : cur->zero;
+		level++;
+	} while (next != NULL && node->length != level/8);
 	
+	if (next != NULL) {
+		// data is identical (at least it should be if it exhausted all bits)
+		// TODO: increment quantity counter?
+		return next->id;
+	} else {
+		node->id = newID;
+		node->parent = cur;
+		if (curBit) { 
+			cur->one = node;
+		} else { 
+			cur->zero = node;
+		}
+		return node->id;
+	}
+}
+
+bool bst_delete(node_t* root, char *id) {
+	return false;
+}
+node_t* bst_get(node_t* root, char* id) {
+	//
 	return NULL;
 }
 
-bool bst_delete(char *id) {
-	return false;
-}
-
-node_t* bst_search(void *data, long length) {
+node_t* bst_search(node_t* root, void *data, long length) {
 	//
 	return NULL;
 }
--- bit_search_tree.h
+++ bit_search_tree.h
@@ -8,18 +8,23 @@ typedef struct node {
     long length;
     struct node_t *zero;
     struct node_t *one;
+    struct node_t *parent;
 } node_t;
 
 // initialize tree
 node_t* bst_init();
 // new node
 node_t* bst_newNode(void *data, long length);
+// destory node (dealloc all assoc. memory)
+void bst_destroyNode(node_t *node, bool recursive);
 // add to tree (returns id)
 char* bst_add(node_t* root, node_t *node);
 // delete from tree (returns successful)
-bool bst_delete(char *id);
+bool bst_delete(node_t* root, char *id);
+// get node from tree by id
+node_t* bst_get(node_t* root, char* id);
 // search tree (returns array of related root node and children)
-node_t* bst_search(void *data, long length);
+node_t* bst_search(node_t* root, void *data, long length);
 // serialize tree (saves tree to file)
 // deserialize tree (loads tree from file)
 
